a9360d940392d2f7715055127facda570ddc4893
Chanisara <chanisara.sra@gmail.com>
2025-07-21 13:48:17 +0700

    refactor ex-5: update character display to use index-based iteration

--- ex-5.js
+++ ex-5.js
@@ -1,6 +1,6 @@
 // Exercise #5: Concatenate the Alphabets
 let companyName = "TechUp";
 
-for (let letter of companyName) {
-    console.log(`character is ${letter}`);
+for (let i=0; i<companyName.length; i=i+1){
+    console.log(`Number ${i+1} character is ${companyName[i]}`);
 };
